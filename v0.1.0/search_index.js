2d2beb048a2cb7f1b55c8562087a31088dd20d99
Documenter.jl <documenter@juliadocs.github.io>
2021-03-07 07:26:27 +0000

    build based on f323817

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ModernGraphs","category":"page"},{"location":"#ModernGraphs","page":"Home","title":"ModernGraphs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ModernGraphs]","category":"page"}]
+}
